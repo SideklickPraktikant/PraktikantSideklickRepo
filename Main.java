0c2047c8411f73fb85f7faf80c041221caaf11df
SideklickPraktikant <praktikant@sideklick.solutions>
2023-10-12 15:27:15 +0200

    changed text

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ import java.util.Scanner;
                                                                                                                                    
 public class Main {                                                                                                          
     public static void main(String[] args) {                                                                                       
-        System.out.println("Das wird Merge nummer 1");                                                                                      
+        System.out.println("Hier habe ich die Datei auch bearbeitet");                                                                                      
     }                                                                                                                                                                                                                                                        
 }
