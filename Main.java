ecc8f0606ef96553a9f9fbef5da4a7040c47b3af
SideklickPraktikant <praktikant@sideklick.solutions>
2023-10-13 09:44:46 +0200

    change

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
                                                                                                            
 public class Main {                                                                                                          
     public static void main(String[] args) {                                                                                       
-        System.out.println("Hier habe ich die Datei auch bearbeitet");                                                                                      
+        System.out.println("Erste datei");
     }                                                                                                                                                                                                                                                        
 }
