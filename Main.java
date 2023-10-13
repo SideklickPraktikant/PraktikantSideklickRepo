9c907d97934306385feeb1608f247a63c23f7ae6
SideklickPraktikant <praktikant@sideklick.solutions>
2023-10-13 09:10:32 +0200

    commit

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
-import java.util.Scanner;                                                                                                          
-                                                                                                                                   
+                                                                                                           
 public class Main {                                                                                                          
     public static void main(String[] args) {                                                                                       
         System.out.println("Hier habe ich die Datei auch bearbeitet");                                                                                      
